2a7861d8db3215ba91203f6d880cb6a6e1ac9895
肖楠 <7367501@qq.com>
2020-10-21 22:08:42 +0800

    提交工程

---
+++ Assets/NewUnlitShader.shader
@@ -0,0 +1,66 @@
+﻿Shader "Unlit/NewUnlitShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+	//	_shadowmap("shadowmap", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+				float4 worldpos:texcoord2;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			uniform float4x4 vp;
+			uniform sampler2D _shadowmap;
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+				o.worldpos = mul(unity_ObjectToWorld, v.vertex);
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+
+				float4 pos = mul(vp, i.worldpos);
+				pos.xyz /= pos.w;
+
+				float4 s = tex2D(_shadowmap, pos*0.5 + 0.5 - vp[0][0]*0.01);
+				return col*1-s.r;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/view.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class view : MonoBehaviour
+{
+
+    public Camera cam;
+    public Matrix4x4 m;
+    public float distance;
+    public Light light;
+
+    public float size;
+
+
+    public Camera lightcam;
+
+    public RenderTexture shadowmap;
+
+    public Material pcf;
+    public void Update()
+    {
+       
+    }
+    // Start is called before the first frame update
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 100), ""))
+        {
+            Debug.LogError(cam.projectionMatrix);
+            Debug.LogError(cam.worldToCameraMatrix);
+          
+
+        }
+        m = cam.previousViewProjectionMatrix;
+    }
+
+    private void OnDrawGizmos()
+    {
+        var point =transform.position+ cam.transform.forward * distance;
+        //center
+        var fov = cam.fieldOfView / 2;
+        var far = transform.position + cam.transform.forward * cam.farClipPlane;
+        var near = transform.position + cam.transform.forward * cam.nearClipPlane;
+        float asct = Screen.width / Screen.height;
+        //tan
+
+       // Debug.LogError(asct);
+
+
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(far, 0.5f);
+        Gizmos.DrawLine(cam.transform.position, far);
+
+
+        //
+        
+
+
+
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(point, 0.5f);
+        Gizmos.DrawLine(cam.transform.position, point);
+
+
+
+       
+
+
+        if(lightcam==null)
+        {
+            CreatLight();
+        }
+
+
+        UpdateCamera(point);
+
+        UpdateMartix();
+
+
+
+
+    }
+
+
+    public void CreatLight()
+    {
+        lightcam = new GameObject("light").AddComponent<Camera>();
+        lightcam.orthographic = true;
+        lightcam.orthographicSize = 15;
+        lightcam.cullingMask = 1<<LayerMask.NameToLayer("cast");
+       // lightcam.aspect = 1;
+    }
+
+    public void UpdateCamera(Vector3 pos)
+    {
+        lightcam.transform.position = pos;
+        lightcam.transform.forward = light.transform.forward;
+        lightcam.transform.position += lightcam.transform.forward * -100;
+        lightcam.farClipPlane = 500;
+        if (lightcam.targetTexture == null)
+        {
+            shadowmap = new RenderTexture(1024, 1024, 0);
+            shadowmap.format = RenderTextureFormat.R8;
+            lightcam.targetTexture = shadowmap;
+            Shader.SetGlobalTexture("_shadowmap", lightcam.targetTexture);
+        }
+    }
+    public void  UpdateMartix()
+    {
+
+        var vp = GL.GetGPUProjectionMatrix(lightcam.projectionMatrix,false)*lightcam.worldToCameraMatrix ;
+        Shader.SetGlobalMatrix("vp", vp);
+        
+        if (pcf==null)
+        {
+            Graphics.Blit()
+        }
+      
+
+    }
+
+}
